4e684a38e45ac11518d835311efdf20a35486357
PINTFORGE <29993075+pintforge@users.noreply.github.com>
2020-08-05 22:30:08 +0100

    Add files via upload

---
+++ l10n_ma_hr_payroll/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Moroccan Payroll',
+    'category': 'Human  Resources',
+    'depends': ['hr_payroll'],
+    'description': """
+Moroccan Payroll Salary Rules.
+============================
+
+    -Configuration of hr_payroll for Morocco localization
+    -All main contributions rules for Morocco payslip.
+    * New payslip report
+    * Employee Contracts
+    * Allow to configure Basic / Gross / Net Salary
+    * Employee PaySlip
+    * Allowance / Deduction
+    * Integrated with Leaves Management
+    - Payroll Advice and Report
+    - Yearly Salary by Head and Yearly Salary by Employee Report
+    """,
+    'data': [
+         'views/l10n_ma_hr_payroll_view.xml',
+         'data/l10n_ma_hr_payroll_data.xml',
+         'security/ir.model.access.csv',
+         'views/l10n_ma_hr_payroll_report.xml',
+         'data/l10n_ma_hr_payroll_sequence_data.xml',
+         'views/report_payslip_details_template.xml',
+         'report/payment_advice_report_view.xml',
+         'report/payslip_report_view.xml',
+         'views/report_payroll_advice_template.xml',
+     ],
+    'demo': [],
+}
---
+++ l10n_ma_hr_payroll/report/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from . import report_payslip_details
+from . import report_payroll_advice
+from . import payment_advice_report
+from . import payslip_report
