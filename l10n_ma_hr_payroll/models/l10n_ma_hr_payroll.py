1f050bc258b1379cacd7d892d7561f728eb447a7
PINTFORGE <29993075+pintforge@users.noreply.github.com>
2020-08-06 16:03:17 +0100

    Update l10n_ma_hr_payroll.py

--- l10n_ma_hr_payroll/models/l10n_ma_hr_payroll.py
+++ l10n_ma_hr_payroll/models/l10n_ma_hr_payroll.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime
 from odoo import api, fields, models, _
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
